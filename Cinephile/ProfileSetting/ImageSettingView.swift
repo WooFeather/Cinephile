83eb6bfa481ea709ce599d70b2998e3247976275
Woohyun Jo <bluewing1004@gmail.com>
2025-01-25 14:54:46 +0900

    [Design] ImageSettingView 이미지뷰 세팅

--- Cinephile/ProfileSetting/ImageSettingView.swift
+++ Cinephile/ProfileSetting/ImageSettingView.swift
@@ -9,5 +9,38 @@ import UIKit
 
 class ImageSettingView: BaseView {
 
+    private let cameraImageView = UIImageView()
+    let imageSettingButton = ImageButton()
     
+    override func configureHierarchy() {
+        [imageSettingButton, cameraImageView].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func configureLayout() {
+        imageSettingButton.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(36)
+            make.centerX.equalTo(self.snp.centerX)
+            make.size.equalTo(100)
+        }
+        
+        cameraImageView.snp.makeConstraints { make in
+            make.top.equalTo(imageSettingButton.snp.bottom).offset(-30)
+            make.trailing.equalTo(imageSettingButton.snp.trailing)
+            make.size.equalTo(25)
+        }
+    }
+    
+    override func configureView() {
+        imageSettingButton.isUserInteractionEnabled = false
+        
+        cameraImageView.image = UIImage(systemName: "camera.fill")?.withAlignmentRectInsets(UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4))
+        cameraImageView.contentMode = .center
+        cameraImageView.backgroundColor = .cineAccent
+        cameraImageView.tintColor = .cinaWhite
+        DispatchQueue.main.async {
+            self.cameraImageView.layer.cornerRadius = self.cameraImageView.frame.width / 2
+        }
+    }
 }
